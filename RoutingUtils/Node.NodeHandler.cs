7a17e4a54ffa5cfa6abb81295666f6ff13cb9ede
machonky <dev@macrobert.com>
2017-03-18 21:16:37 +0800

    Refactor the StateHandlerFactory

--- RoutingUtils/Node.NodeHandler.cs
+++ RoutingUtils/Node.NodeHandler.cs
@@ -15,14 +15,16 @@ namespace CoreDht
             IHandle<OperationComplete>
         {
             protected Node Node { get; }
-
-
+            
             public NodeHandler(Node node)
             {
                 Node = node;
                 var stateFactory = new StateHandlerFactory(Node);
                 foreach (var messageType in stateFactory.TriggerMessageTypes)
                 {
+                    // Ordinarily the Repository can discover all trigger messages in the RepositoryItem
+                    // but since there are multiple types of repo items in this repo, we need to give it 
+                    // some assistance. The factory tells the repo what triggers it can handle.
                     TriggerMessageTypes.Add(messageType);
                 }
                 RepoItemFactory = stateFactory.CreateHandler;
--- RoutingUtils/Node.StateHandler.cs
+++ RoutingUtils/Node.StateHandler.cs
@@ -40,55 +40,5 @@ namespace CoreDht
                 }
             }
         }
-
-        public class StateHandlerFactory
-        {
-            private static readonly Dictionary<Type, Func<Guid, Node, StateHandler>> _stateHandlers;
-
-            static StateHandlerFactory()
-            {
-                Func<Guid, Node, StateHandler>
-                    joinNetworkHandler = (guid, node) => new JoinNetworkHandler(guid, node),
-                    findSuccessorHandler = (guid, node) => new FindSuccessorToHashHandler(guid, node),
-                    getFingerTableHandler = (guid, node) => new GetFingerTableHandler(guid, node),
-                    getSuccessorHandler = (guid, node) => new GetSuccessorListHandler(guid, node);
-
-                _stateHandlers =
-                    new Dictionary<Type, Func<Guid, Node, StateHandler>>
-                    {
-                        {typeof(JoinNetwork), joinNetworkHandler},
-                        {typeof(JoinNetwork.Await), joinNetworkHandler},
-                        {typeof(FindSuccessorToHash), findSuccessorHandler},
-                        {typeof(FindSuccessorToHash.Await), findSuccessorHandler},
-                        {typeof(GetFingerTable), getFingerTableHandler},
-                        {typeof(GetFingerTable.Await), getFingerTableHandler},
-                        {typeof(GetSuccessorTable),getSuccessorHandler},
-                        {typeof(GetSuccessorTable.Await),getSuccessorHandler},
-                    };
-            }
-
-            public StateHandlerFactory(Node node)
-            {
-                Node = node;
-            }
-
-            public Node Node { get; }
-
-            public IEnumerable<Type> TriggerMessageTypes => _stateHandlers.Keys;
-
-            public StateHandler CreateHandler(Message msg)
-            {
-                var cMsg = msg as ICorrelatedMessage<Guid>;
-                if (cMsg != null)
-                {
-                    Func<Guid, Node, StateHandler> factory;
-                    if (_stateHandlers.TryGetValue(msg.GetType(), out factory))
-                    {
-                        return factory(cMsg.CorrelationId, Node);
-                    }
-                }
-                throw new NotImplementedException($"No state handler for {msg.GetType()}");
-            }
-        }
     }
 }
