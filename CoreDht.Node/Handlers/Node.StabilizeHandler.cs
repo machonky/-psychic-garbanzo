fa52cf877f57143b1f1ccc5148fbea6d51c814c4
machonky <dev@macrobert.com>
2017-05-17 08:54:17 +0800

    StabilizeFromSuccessor - happy path

--- CoreDht.Node/Handlers/Node.StabilizeHandler.cs
+++ CoreDht.Node/Handlers/Node.StabilizeHandler.cs
@@ -40,23 +40,37 @@ namespace CoreDht.Node
                         var predecessorHash = reply.Predecessor.RoutingHash;
                         var successorHash = _node.Successor.RoutingHash;
 
-                        // We need to check the order of the predecessor or the successor before adopting a value.
+                        // We need to check the order of the predecessor or the successor before adopting a value
+                        // as nodes are racing to join the seed node - our last query may be out of date, but not by much
                         if (thisHash.IsBetween(predecessorHash, successorHash))
                         {
-                            // StabilizeFromSuccessor...
-                            // No ordering change. We can adopt and tell the successor to rectify
-                            //_node.Predecessor = reply.Predecessor;
-                            //_node.SuccessorTable.Copy(reply.SuccessorTableEntries);
+                            // No ordering change. We can adopt the predecessor values and tell the successor to rectify
+                            StabilizeFromSuccessor(reply);
                         }
                         else if (predecessorHash.IsBetween(thisHash, successorHash))
                         {
                             // another node beat this one to join to the successor, changing the ordering...
-                            // StabilizeFromPredecessor...
-                            // The predecessor is a better successor than the current one.
-                            // We should quit this operation and stabilize against the precedessor
+                            StabilizeFromPredecessor();
                         }
                     })
                     .Run(opId);
+            }
+
+            private void StabilizeFromSuccessor(StabilizeReply reply)
+            {
+                _node.Predecessor = reply.Predecessor;
+                _node.SuccessorTable.Copy(reply.SuccessorTableEntries);
+
+                var nextMsg = new InitRectify();
+                var forwardingSocket = _node.ForwardingSockets[_node.Identity.HostAndPort];
+                _node.Marshaller.Send(nextMsg, forwardingSocket);
+            }
+
+            private void StabilizeFromPredecessor()
+            {
+                // StabilizeFromPredecessor...
+                // The predecessor is a better successor than the current one.
+                // We should quit this operation and stabilize against the precedessor
             }
 
             private void ScheduleNextStabilize()
