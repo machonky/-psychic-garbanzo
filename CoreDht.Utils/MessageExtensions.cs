7ddeecda426c9c3ad8d8b54ce59f4036ba6b644d
machonky <dev@macrobert.com>
2017-05-27 13:16:28 +0800

    MessageExtensions TypeName

---
+++ CoreDht.Utils/MessageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreMemoryBus.Messages;
+
+namespace CoreDht.Utils
+{
+    public static class MessageExtensions
+    {
+        public static string TypeName(this Message msg)
+        {
+            return msg.GetType().Name;
+        }
+    }
+}
