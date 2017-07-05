26ab79005e645da38fd72db6267d01811dea8018
machonky <dev@macrobert.com>
2017-07-05 21:01:30 +0800

    Disposable Action

--- CoreDht.Utils/DisposableStack.cs
+++ CoreDht.Utils/DisposableStack.cs
@@ -38,4 +38,32 @@ namespace CoreDht.Utils
         }
         #endregion
     }
+
+    public class DisposableAction : IDisposable
+    {
+        private readonly Action _disposeAction;
+
+        public DisposableAction(Action initAction, Action disposeAction)
+        {
+            initAction?.Invoke();
+            _disposeAction = disposeAction;
+        }
+        public DisposableAction(Action disposeAction) : this(null, disposeAction)
+        { }
+
+        #region IDisposable Support
+
+        private bool _isDisposed = false;
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                _disposeAction();
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
+    }
 }
